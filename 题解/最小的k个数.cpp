5bd1254e2481a259ec7ad3049e552a752182d679
maoqifan1 <maoqifanai@foxmail.com>
2020-03-20 13:55:26 +0800

    最小k个数(非常简单=。=)

---
+++ 题解/最小的k个数.cpp
@@ -0,0 +1,25 @@
+// 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+
+// 示例 1：
+
+// 输入：arr = [3,2,1], k = 2
+// 输出：[1,2] 或者 [2,1]
+// 示例 2：
+
+// 输入：arr = [0,1,2,1], k = 1
+// 输出：[0]
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getLeastNumbers(vector<int> &arr, int k)
+    {
+        sort(arr.begin(), arr.end()); // 排序
+        arr.erase(arr.begin() + k,arr.end()); // 删除不需要的元素
+        return arr;
+    }
+};
